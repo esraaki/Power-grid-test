b73cb048beb9434361d3a00462e3e4b42ce30cf7
esraaki <esraasalah4242@gmail.com>
2026-09-20 15:46:49 +0300

    begin

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MotionConfig } from 'motion/react';
+import App from './App';
+import './styles/global.css';
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><MotionConfig reducedMotion="user"><App/></MotionConfig></React.StrictMode>);
